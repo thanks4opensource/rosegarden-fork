thanks4opensource/rosegarden-fork

commit 6f640bc9af218c3b0da297c86cf16ebbf6bee5b0

Feature Request #501 Unified Matrix/Percussion Editor
Changes Matrix Editor to display/edit percussion segments as per
previous Percussion Editor. No change to display/editing of normal
pitched instrument segments.
- Removed Percussion Editor because now identical to Matrix Editor
- Matrix Editor menu "View -> Notes -> Show percussion durations"
  toggle to allow editing of percussion segment note durations as per
  percussion segments in pre-commit Matrix Editor.
- In normal "Show percussion durations" off mode (display of
  percussion notes as diamonds) grid click to nearest grid beat
  instead of left/earlier beat. Notes are conceptually zero-duration
  on the beat and was annoying to aim for a grid beat but click
  slightly to the left of grid line and have snap to previous beat.

Code changes to implement the above include:
- Removed of Percussion Editor menus/actions in all GUI windows, code
  and .rc files.
- Create new Matrix Editor "Edit -> Notes" sub-menu for new "Show
  percussion durations" and existing "Show note names" buttons.
- Removed of m_drumMode and constructor argument in MatrixView class
  (was unchanging mode differentiating Matrix Editor from Percussion
  Matrix Editor, now dynamically changed per active segment type).
- Save/restore "Show percussion durations" in/from QSettings.
- Added MatrixView::slotPercussionDurations() method.
- Check for no user change of GUI setting in MatrixView::slotHighlight().
- MatrixScene::setSegments() constructs MatrixViewSegments based on
  percussion-vs-pitched type of segments.
- Added MatrixScene::m_observerAdded member to avoid multiple calls
  to Composition::addObserver() (had seen happening during testing,
  can cause not only inefficient multiple calls but possible problems
  with multiple construction of objects, etc.)
- Added MatrixScene::trackChanged() observer to update MatrixEditor in
  edge case of segment changing from percussion to normal or vice-versa.

src/gui/editors/matrix/MatrixView.cpp
@@ -127,17 +127,14 @@ namespace Rosegarden
 
 MatrixView::MatrixView(RosegardenDocument *doc,
                  std::vector<Segment *> segments,
-                 bool drumMode,
                  QWidget *parent) :
     EditViewBase(segments, parent),
     m_quantizations(BasicQuantizer::getStandardQuantizations()),
-    m_drumMode(drumMode),
     m_inChordMode(false)
 {
     m_document = doc;
-    m_matrixWidget = new MatrixWidget(m_drumMode);
+    m_matrixWidget = new MatrixWidget(this);
     setCentralWidget(m_matrixWidget);
-    m_matrixWidget->setSegments(doc, segments);
 
     // Many actions are created here
     // ??? MEMORY LEAK (confirmed)
@@ -146,6 +143,13 @@ MatrixView::MatrixView(RosegardenDocument *doc,
     setupActions();
 
     createMenusAndToolbars("matrix.rc");
+
+    // Ensure that initial display of any percussion segments is
+    // correct with respect to View -> Notes -> Show percussion durations.
+    // Following two calls must be done in order, and both
+    // after createMenusAndToolbars()
+    setShowPercussionDurations();
+    m_matrixWidget->setSegments(doc, segments);
 
     findToolbar("General Toolbar");
 
@@ -223,7 +227,6 @@ MatrixView::MatrixView(RosegardenDocument *doc,
         static_cast<MatrixScene::HighlightType>(
             settings.value("highlight_type",
                            MatrixScene::HT_BlackKeys).toInt());
-    settings.endGroup();
     switch (chosenHighlightType) {
     case MatrixScene::HT_BlackKeys:
         findAction("highlight_black_notes")->setChecked(true);
@@ -235,6 +238,7 @@ MatrixView::MatrixView(RosegardenDocument *doc,
         findAction("highlight_black_notes")->setChecked(true);
         break;
     }
+
     settings.endGroup();
 
     if (segments.size() > 1) {
@@ -243,17 +247,13 @@ MatrixView::MatrixView(RosegardenDocument *doc,
         leaveActionState("have_multiple_segments");
     }
 
-    if (m_drumMode) {
-        enterActionState("in_percussion_matrix");
-    } else {
-        enterActionState("in_standard_matrix");
-    }
+    enterActionState("in_standard_matrix");
 
     // Restore window geometry and toolbar/dock state
     settings.beginGroup(WindowGeometryConfigGroup);
-    QString modeStr = (m_drumMode ? "Percussion_Matrix_View_Geometry" : "Matrix_View_Geometry");
+    QString modeStr = "Matrix_View_Geometry";
     this->restoreGeometry(settings.value(modeStr).toByteArray());
-    modeStr = (m_drumMode ? "Percussion_Matrix_View_State" : "Matrix_View_State");
+    modeStr = "Matrix_View_State";
     this->restoreState(settings.value(modeStr).toByteArray());
     settings.endGroup();
 
@@ -289,6 +289,19 @@ MatrixView::~MatrixView()
 }
 
 void
+MatrixView::setShowPercussionDurations()
+{
+    QSettings settings;
+    settings.beginGroup(MatrixViewConfigGroup);
+    bool showPercussionDurations = qStrToBool(settings.value(
+                                              "show_percussion_durations"));
+    findAction("show_percussion_durations")->setChecked(
+                                             showPercussionDurations);
+    settings.endGroup();
+    m_matrixWidget->setShowPercussionDurations(showPercussionDurations);
+}
+
+void
 MatrixView::launchRulers(std::vector<Segment *> segments)
 {
     if (!m_matrixWidget)
@@ -309,9 +322,9 @@ MatrixView::closeEvent(QCloseEvent *event)
     // Save window geometry and toolbar/dock state
     QSettings settings;
     settings.beginGroup(WindowGeometryConfigGroup);
-    QString modeStr = (m_drumMode ? "Percussion_Matrix_View_Geometry" : "Matrix_View_Geometry");
+    QString modeStr = "Matrix_View_Geometry";
     settings.setValue(modeStr, this->saveGeometry());
-    modeStr = (m_drumMode ? "Percussion_Matrix_View_State" : "Matrix_View_State");
+    modeStr = "Matrix_View_State";
     settings.setValue(modeStr, this->saveState());
     settings.endGroup();
 
@@ -539,6 +552,7 @@ MatrixView::setupActions()
     createAction("donate", SLOT(slotDonate()));
 
     createAction("show_note_names", SLOT(slotShowNames()));
+    createAction("show_percussion_durations", SLOT(slotPercussionDurations()));
     createAction("highlight_black_notes", SLOT(slotHighlight()));
     createAction("highlight_triads", SLOT(slotHighlight()));
     // add the highlight actions to an ActionGroup
@@ -1665,25 +1679,49 @@ MatrixView::slotShowNames()
 }
 
 void
+MatrixView::slotPercussionDurations()
+{
+    bool show = findAction("show_percussion_durations")->isChecked();
+    RG_DEBUG << "show percussion durations:" << show;
+    QSettings settings;
+    settings.beginGroup(MatrixViewConfigGroup);
+    settings.setValue("show_percussion_durations", show);
+    settings.endGroup();
+    m_matrixWidget->setShowPercussionDurations(show);
+    static constexpr bool onlyPercussion = true;
+    m_matrixWidget->getScene()->updateAll(onlyPercussion);
+}
+
+void
 MatrixView::slotHighlight()
 {
+    QSettings settings;
+    settings.beginGroup(MatrixViewConfigGroup);
+    MatrixScene::HighlightType previousHighlightType =
+        static_cast<MatrixScene::HighlightType>(
+            settings.value("highlight_type",
+                           MatrixScene::HT_BlackKeys).toInt());
+    MatrixScene::HighlightType newHighlightType = previousHighlightType;
+
     const QObject *s = sender();
     QString name = s->objectName();
     if (name == "highlight_black_notes") {
         RG_DEBUG << "highlight black notes";
-        QSettings settings;
-        settings.beginGroup(MatrixViewConfigGroup);
         settings.setValue("highlight_type", MatrixScene::HT_BlackKeys);
-        settings.endGroup();
+        newHighlightType = MatrixScene::HT_BlackKeys;
     }
-    if (name == "highlight_triads") {
+    else if (name == "highlight_triads") {
         RG_DEBUG << "highlight triads";
-        QSettings settings;
-        settings.beginGroup(MatrixViewConfigGroup);
         settings.setValue("highlight_type", MatrixScene::HT_Triads);
-        settings.endGroup();
+        newHighlightType = MatrixScene::HT_Triads;
     }
-    m_matrixWidget->getScene()->updateAll();
+
+    settings.endGroup();
+
+    // Only do if changed.
+    if (newHighlightType != previousHighlightType) {
+        m_matrixWidget->getScene()->updateAll();
+    }
 }
 
 void

src/gui/editors/matrix/MatrixView.h
@@ -63,7 +63,6 @@ class MatrixView : public EditViewBase,
 public:
     MatrixView(RosegardenDocument *doc,
                   std::vector<Segment *> segments,
-                  bool drumMode,
                   QWidget *parent = nullptr);
 
     ~MatrixView() override;
@@ -193,6 +192,7 @@ protected slots:
     // view
     void slotShowNames();
     void slotHighlight();
+    void slotPercussionDurations();
 
     void slotShowContextHelp(const QString &);
 
@@ -281,11 +281,11 @@ private:
     std::vector<timeT> m_quantizations;
     std::vector<timeT> m_snapValues;
 
+    void setShowPercussionDurations();
     void setupActions();
     void initActionsToolbar();
     void initRulersToolbar();
 
-    bool m_drumMode;
     bool m_Thorn;
     bool m_inChordMode;
 

data/rc/matrix.rc
@@ -30,7 +30,11 @@
 
   <Menu name="View">
     <text>&amp;View</text>
-    <Action name="show_note_names" text="Show note names" checked="false" />
+    <Menu name="Notes">
+      <text>Notes</text>
+      <Action name="show_note_names" text="Show note names" checked="false" />
+      <Action name="show_percussion_durations" text="Show percussion durations" checked="false" />
+    </Menu>
     <Menu name="Highlight">
       <text>&amp;Highlight</text>
       <Action name="highlight_black_notes" text="Highlight black notes" checked="false" />

data/rc/notation.rc
@@ -141,7 +141,6 @@
       <text>Edit &amp;With</text>
       <Action name="open_in_event_list" text="Open in &amp;Event List Editor" icon="eventlist" />
       <Action name="open_in_matrix" text="Open in Matri&amp;x Editor" icon="matrix" />
-      <Action name="open_in_percussion_matrix" text="Open in &amp;Percussion Matrix Editor" icon="matrix-percussion" />
       <Action name="open_in_pitch_tracker" text="Open in &amp;Pitch Tracker" icon="pitchtracker" />
     </Menu>
   <Separator/>

data/rc/rosegardenmainwindow.rc
@@ -180,7 +180,6 @@
       <Action name="edit_default" text="Open in &amp;Default Editor" shortcut="Return" />
     <Separator/>
       <Action name="edit_matrix" text="Open in Matri&amp;x Editor" icon="matrix" shortcut="M" />
-      <Action name="edit_percussion_matrix" text="Open in &amp;Percussion Matrix Editor" icon="matrix-percussion" shortcut="D" />
       <Action name="edit_notation" text="Open in &amp;Notation Editor" icon="notation" shortcut="N" />
       <Action name="edit_event_list" text="Open in &amp;Event List Editor" icon="eventlist" shortcut="E" />
       <Action name="edit_pitch_tracker" text="Open in &amp;Pitch Tracker" icon="pitchtracker" shortcut="I" />
@@ -329,7 +328,6 @@
 <ToolBar name="Editors Toolbar" newline="true">
   <text>Editors Toolbar</text>
   <Action name="edit_matrix" />
-  <Action name="edit_percussion_matrix" />
   <Action name="edit_notation" />
   <Action name="edit_event_list" />
   <Action name="edit_pitch_tracker" />
@@ -405,7 +403,6 @@
     <Action name="edit_copy"/>
     <Action name="edit_default"/>
     <Action name="edit_matrix"/>
-    <Action name="edit_percussion_matrix"/>
     <Action name="edit_notation"/>
     <Action name="edit_event_list"/>
     <Action name="edit_pitch_tracker"/>
@@ -453,7 +450,6 @@
   <disable>
     <Action name="edit_notation"/>
     <Action name="edit_matrix"/>
-    <Action name="edit_percussion_matrix"/>
     <Action name="edit_event_list"/>
     <Action name="edit_pitch_tracker"/>
     <Action name="quantize_selection"/>

data/rc/segmenttool.rc
@@ -8,7 +8,6 @@
   <Action name="edit_default" text="Open in &amp;Default Editor" shortcut="Return" />
 <Separator/>
   <Action name="edit_matrix" text="Open in Matri&amp;x Editor" icon="matrix" shortcut="M" />
-  <Action name="edit_percussion_matrix" text="Open in &amp;Percussion Matrix Editor" icon="matrix-percussion" shortcut="D" />
   <Action name="edit_notation" text="Open in &amp;Notation Editor" icon="notation" shortcut="N" />
   <Action name="edit_event_list" text="Open in &amp;Event List Editor" icon="eventlist" shortcut="E" />
   <Action name="edit_pitch_tracker" text="Open in &amp;Pitch Tracker" icon="pitchtracker" shortcut="I" />

src/gui/application/LircCommander.cpp
@@ -78,8 +78,6 @@ LircCommander::LircCommander(LircClient *lirc, RosegardenMainWindow *rgGUIApp)
             m_rgGUIApp, &RosegardenMainWindow::slotHelpAbout );
     connect(this, &LircCommander::editInMatrix,
             m_rgGUIApp, &RosegardenMainWindow::slotEditInMatrix );
-    connect(this, &LircCommander::editInPercussionMatrix,
-            m_rgGUIApp, &RosegardenMainWindow::slotEditInPercussionMatrix );
     connect(this, &LircCommander::editInEventList,
             m_rgGUIApp, &RosegardenMainWindow::slotEditInEventList );
     connect(this, &LircCommander::editAsNotation,
@@ -99,7 +97,6 @@ LircCommander::command LircCommander::commands[] =
         { "EDITEVLIST",         cmd_editInEventList },
         { "EDITMATRIX",         cmd_editInMatrix },
         { "EDITNOTATION",       cmd_editAsNotation },
-        { "EDITPMATRIX",        cmd_editInPercussionMatrix },
         { "FORWARD",            cmd_fastForward },
         { "FORWARDTOEND",       cmd_fastForwardToEnd },
         { "PLAY",               cmd_play },
@@ -190,9 +187,6 @@ void LircCommander::slotExecute(const char *command)
             break;
         case cmd_editInMatrix:
             emit editInMatrix();
-            break;
-        case cmd_editInPercussionMatrix:
-            emit editInPercussionMatrix();
             break;
         case cmd_editAsNotation:
             emit editAsNotation();

src/gui/application/LircCommander.h
@@ -61,7 +61,6 @@ signals:
     void aboutrg();
     void editInEventList();
     void editInMatrix();
-    void editInPercussionMatrix();
     void editAsNotation();
     void quit();
     void closeTransport();
@@ -95,7 +94,6 @@ private:
         cmd_aboutrg,
         cmd_editInEventList,
         cmd_editInMatrix,
-        cmd_editInPercussionMatrix,
         cmd_editAsNotation,
         cmd_quit,
         cmd_closeTransport,

commit 7dfa5c70437fbb8094e37ee04a72ccf2fb4b397a

Fixed missing Matrix Editor "Add Control Ruler" menu items
- Bug introduced in 6f640bc9a Feature Request #501 Unified Matrix/Percussion
  Editor
- Caused by moving call to MatrixView::setupActions() before call to
  MatrixWidget::setSegments() in MatrixView constructor.
- Done for other reasons, worked without problems except undiscovered
  bug, populating "Add Control Ruler" in setupActions() requires
  MatrixWidget's initialization of segments, etc.
- Fixed by breaking out code from MatrixView::setupActions() into
  setupAddRulerActions() and calling former before and latter after
  MatrixWidget::setSegments().
- Note simply calling existing MatrixView::setupActions() after
  MatrixWidget::setSegments() causes immediate crash bringing up ME
  due to other, interlocking initialization order, problems.

src/gui/editors/matrix/MatrixView.cpp
@@ -153,6 +153,16 @@ MatrixView::MatrixView(RosegardenDocument *doc,
     setShowNoteColorTypeAndAll();
     m_matrixWidget->setSegments(doc, segments);
 
+    // Broken out of setupActions().
+    // Must be after MatrixWidget::setSegments() because populating
+    // "Add control ruler" menu requires getCurrentDevice() which
+    // in turn calls MatrixWidget::getCurrentDevice() which calls
+    // MatrixWidget::getCurrentSegment(), and they require MatrixWidget
+    // segment initialization.
+    // Can't just move call to setupActions() here due to other
+    // interlocking initialization order requirements.
+    setupAddRulerActions();
+
     findToolbar("General Toolbar");
 
     m_Thorn = ThornStyle::isEnabled();
@@ -499,44 +509,8 @@ MatrixView::setupActions()
     createAction("jog_left", SLOT(slotJogLeft()));
     createAction("jog_right", SLOT(slotJogRight()));
 
-    QMenu *addControlRulerMenu = new QMenu;
-    Controllable *c =
-        dynamic_cast<MidiDevice *>(getCurrentDevice());
-    if (!c) {
-        c = dynamic_cast<SoftSynthDevice *>(getCurrentDevice());
-    }
-
-    if (c) {
-
-        const ControlList &list = c->getControlParameters();
-
-        QString itemStr;
-
-        for (ControlList::const_iterator it = list.begin();
-             it != list.end(); ++it) {
-
-            // Pitch Bend is treated separately now, and there's no
-            // point in adding "unsupported" controllers to the menu,
-            // so skip everything else
-            if (it->getType() != Controller::EventType) continue;
-
-            const QString hexValue =
-                QString::asprintf("(0x%x)", it->getControllerNumber());
-
-            // strings extracted from data files must be QObject::tr()
-            itemStr = QObject::tr("%1 Controller %2 %3")
-                .arg(QObject::tr(it->getName().c_str()))
-                .arg(it->getControllerNumber())
-                .arg(hexValue);
-
-            addControlRulerMenu->addAction(itemStr);
-        }
-    }
-
-    connect(addControlRulerMenu, &QMenu::triggered,
-            this, &MatrixView::slotAddControlRuler);
-
-    findAction("add_control_ruler")->setMenu(addControlRulerMenu);
+    // Code in setupAddRulerActions() below was here.
+    // See call in constructor for reason why moved.
 
     // (ported from NotationView)
     //JAS insert note section is a rewrite
@@ -654,6 +628,51 @@ MatrixView::setupActions()
     }
 }
 
+void
+MatrixView::setupAddRulerActions()
+{
+    // See explanation at call in constructor for why broken out of
+    // setupActions() at location commented in that code, above (search
+    // for "setupAddRulerActions()")
+
+    QMenu *addControlRulerMenu = new QMenu;
+    Controllable *c =
+        dynamic_cast<MidiDevice *>(getCurrentDevice());
+    if (!c) {
+        c = dynamic_cast<SoftSynthDevice *>(getCurrentDevice());
+    }
+
+    if (c) {
+        const ControlList &list = c->getControlParameters();
+
+        QString itemStr;
+
+        for (ControlList::const_iterator it = list.begin();
+             it != list.end(); ++it) {
+
+            // Pitch Bend is treated separately now, and there's no
+            // point in adding "unsupported" controllers to the menu,
+            // so skip everything else
+            if (it->getType() != Controller::EventType) continue;
+
+            const QString hexValue =
+                QString::asprintf("(0x%x)", it->getControllerNumber());
+
+            // strings extracted from data files must be QObject::tr()
+            itemStr = QObject::tr("%1 Controller %2 %3")
+                .arg(QObject::tr(it->getName().c_str()))
+                .arg(it->getControllerNumber())
+                .arg(hexValue);
+
+            addControlRulerMenu->addAction(itemStr);
+        }
+    }
+
+    connect(addControlRulerMenu, &QMenu::triggered,
+            this, &MatrixView::slotAddControlRuler);
+
+    findAction("add_control_ruler")->setMenu(addControlRulerMenu);
+}
 
 void
 MatrixView::initActionsToolbar()

src/gui/editors/matrix/MatrixView.h
@@ -287,6 +287,7 @@ private:
     void setShowPercussionDurations();
     void setShowNoteColorTypeAndAll();
     void setupActions();
+    void setupAddRulerActions();
     void initActionsToolbar();
     void initRulersToolbar();
 